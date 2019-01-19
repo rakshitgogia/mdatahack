ad414c803801fd673d98ef36711fd8f625bf9c19
Jen Sheng Wong <jenshengwong@0587394868.wireless.umich.net>
2019-01-18 23:05:48 -0500

    clean df 2304

--- test_v1.py
+++ test_v1.py
@@ -20,7 +20,7 @@ df.drop('Player Name', axis=1, inplace=True)
 # drop useless features
 df.drop(['Period Number', 'Date_obj', 'Start Time', 'End Time', 'Day Name', 'Month Name'], axis=1, inplace=True)
 
-print(df['ID'].value_counts())
+# print(df['ID'].value_counts())
 
 # relabel target
 df.loc[df['Position Name'] == 'Goal Keeper', 'Position Name'] = 'Goalkeeper'
@@ -29,30 +29,54 @@ df.loc[df['Position Name'] == 'Midfield', 'Position Name'] = 'Mid'
 
 # convert Activity Name to practice (0) or game (1)
 df['practice_or_game'] = df['Activity Name'].str.contains('Practice', case=False) == True
-df['practice_or_game'][df['practice_or_game'] == False] = '0'
-df['practice_or_game'][df['practice_or_game'] == True] = '1'
+df.loc[df['practice_or_game'] == False, 'practice_or_game'] = 0
+df.loc[df['practice_or_game'] == True, 'practice_or_game'] = 1
+
+# print(df['practice_or_game'].value_counts())
 
 df.drop('Activity Name', axis=1, inplace=True)
 
 # clean Period Name
 df= df[(df['Period Name'] != "Water Break")]
 
-# Remove misleading practices
+# remove misleading practices
 df = df[(df["Period Name"]).str.contains("OT practice", case=False) == False]
 df = df[(df["Period Name"]).str.contains("11 v 11", case=False) == False]
 df = df[(df["Period Name"]).str.contains("11v11", case=False) == False]
 df = df[(df["Period Name"]).str.contains("9 v 9", case=False) == False]
 df = df[(df["Period Name"]).str.contains("9v9", case=False) == False]
 
-print(df['Period Name'].value_counts())
+# combine all warmups
+df.loc[df['Period Name'].str.contains('war', case=False), 'Period Name'] = 'Warmup'
 
-# Encode features
+# combine all first and session into GAME
+df.loc[df['Period Name'].str.contains('firs', case=False), 'Period Name'] = 'Game'
+df.loc[df['Period Name'].str.contains('secon', case=False), 'Period Name'] = 'Game'
+
+# label encode each Period Name
+# Warmup (0), Game (1), Session (2), Misc. (3)
+df.loc[df['Period Name'] == 'Warmup', 'new_period'] = 0
+df.loc[df['Period Name'] == 'Game', 'new_period'] = 1
+df.loc[df['Period Name'] == 'Session', 'new_period'] = 2
+df['new_period'] = df['new_period'].fillna(value=3)
+
+df.drop('Period Name', axis=1, inplace=True)
+
+# print(df['new_period'].value_counts())
+
+# encode target
 dic = {'Back': 0, 'Forward': 1, 'Mid/Back': 2,
        'Mid': 3, 'Mid/Forward': 4, 'Goalkeeper': 5}
 
 df["Position Name"] = df["Position Name"].apply(lambda x: dic[x])
 
-print(df['Position Name'].value_counts())
+df["ID"] = df["ID"].astype('int8')
+df["practice_or_game"] = df["practice_or_game"].astype('int8')
+df["new_period"] = df["new_period"].astype('int8')
+
+print(df.dtypes)
+
+# print(df['Position Name'].value_counts())
 
 
 # #filter out water breaks
